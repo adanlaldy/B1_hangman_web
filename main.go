e36a6ca7d014965f3eed3745474946c967077fea
Lucille Cénac <lucille.cenac@ynov.com>
2023-01-06 13:43:50 +0100

    button

--- main.go
+++ main.go
@@ -5,24 +5,36 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
 )
+
+type HangmanWeb struct {
+	classic classic.HangManData
+	Nletter string
+}
 
 func HandlePage(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles("./templates/home.html"))
-	input := "t pa bo"
-	data := classic.HangManData{
-		Try:             "",
-		Letter:          input,
-		Randomword:      "",
-		TotalTries:      0,
-		NFormula:        0,
-		Slice:           []string{},
-		SliceRandomword: []string{},
-		Boolean:         true,
+	data := HangmanWeb{
+		classic: classic.HangManData{
+			Try:             "",
+			Letter:          "",
+			Randomword:      strings.ToUpper(classic.Randomword()),
+			TotalTries:      10,
+			NFormula:        0,
+			Slice:           []string{},
+			SliceRandomword: []string{},
+			Boolean:         false,
+		},
 	}
+	data.classic.NFormula = len(classic.Randomword())/2 - 1
+	data.classic.Slice = make([]string, len(data.classic.Randomword))
+	data.classic.SliceRandomword = make([]string, len(data.classic.Randomword))
+	classic.PrintLettersInTheFullSlice(&data.classic)
+	classic.Start(&data.classic)
+	data.Nletter = classic.PrintNLetters(&data.classic)
 	t.Execute(w, data)
 }
-
 func main() {
 	print("http://localhost:8080")
 	http.HandleFunc("/", HandlePage)
