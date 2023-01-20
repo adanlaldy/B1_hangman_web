9ca082c43c22f97376b5c40ccc398dc3301c08a1
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2023-01-20 15:23:44 +0100

    web_update

--- main.go
+++ main.go
@@ -15,9 +15,11 @@ var Data = HangmanWeb{
 	classic: classic.HangManData{
 		Try:             "",
 		Letter:          "",
+		Name:            "",
 		Randomword:      strings.ToUpper(classic.Randomword()),
 		TotalTries:      10,
 		NFormula:        0,
+		Jose:            0,
 		Slice:           []string{},
 		SliceRandomword: []string{},
 		SliceTries:      []string{},
@@ -26,8 +28,6 @@ var Data = HangmanWeb{
 	},
 }
 
-// A faire plus propre
-// L'array qui stock les lettres déjà utilisés commence à beuguer quand il y a beaucoup de lettres ou quand on input des majuscules
 func HandlePage(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles("./templates/home.html"))
 	if r.FormValue("input") != "" {
@@ -41,9 +41,9 @@ func HandlePage(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		if classic.Ifinputisthesame(&Data.classic) == true {
-
 		} else {
-			if classic.IfInputIsTrue(&Data.classic) == false && Data.classic.Boolean == false {
+			if classic.IfInputIsTrue(&Data.classic) == false {
+				Data.classic.Jose++
 				Data.classic.TotalTries--
 				Data.classic.SliceTries = append((Data.classic.SliceTries), Data.classic.Try)
 			} else if Data.classic.Boolean == true {
@@ -58,14 +58,18 @@ func HandlePage(w http.ResponseWriter, r *http.Request) {
 		Tries      int
 		Slice      string
 		SliceTries []string
+		Jose       int
 	}{
 		Data.classic.TotalTries,
 		classic.PrintSlice(&Data.classic),
 		Data.classic.SliceTries,
+		Data.classic.Jose,
 	})
 }
 
 func main() {
+	fs := http.FileServer(http.Dir("jose"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
 	print("http://localhost:8080")
 	Data.classic.NFormula = len(classic.Randomword())/2 - 1
 	Data.classic.Slice = make([]string, len(Data.classic.Randomword))
@@ -73,6 +77,10 @@ func main() {
 	classic.PrintLettersInTheFullSlice(&Data.classic)
 	classic.Start(&Data.classic)
 	classic.PrintNLetters(&Data.classic)
+	Data.classic.Jose = 0
+	/*t := template.Must(template.ParseFiles("./templates/home.html"))
+	Data.classic.Name = (r.FormValue("name"))
+	t.Execute(w, struct {})*/
 	http.HandleFunc("/", HandlePage)
 	http.ListenAndServe(":8080", nil)
 }
